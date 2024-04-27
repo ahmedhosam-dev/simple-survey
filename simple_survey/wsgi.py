6ff6ddf46d429ec6741419aa91d7b29087ff84c1
ahmedhosam-dev <ahmed.hossam.t.11@gmail.com>
2024-04-27 11:42:09 +0300

    Make app var and asign application to it

--- simple_survey/wsgi.py
+++ simple_survey/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_survey.settings')
 
 application = get_wsgi_application()
+
+app = application # Vercel
