8d5cb9dc7b26ea238279c32d7afcc7b014c4896c
Ahmed Hosam <106636932+ahmedhosam-dev@users.noreply.github.com>
2024-04-27 12:48:13 +0300

    Update wsgi.py

--- simple_survey/wsgi.py
+++ simple_survey/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_survey.settings')
 
 application = get_wsgi_application()
-
-app = application # Vercel
